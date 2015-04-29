da3d60253d479c8f0f4a3c3128559be8866a38ff
Pat Downey <pat.downey@gmail.com>
2015-04-30 00:05:32 +0100

    Initial commit of simple task to run an image

---
+++ docker-run/src/main/name/patdowney/gocd/plugin/task/docker/run/DockerRunTask.java
@@ -0,0 +1,126 @@
+/*************************GO-LICENSE-START*********************************
+ * Copyright 2014 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *************************GO-LICENSE-END***********************************/
+
+package name.patdowney.gocd.plugin.task.docker.run;
+
+import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
+import com.thoughtworks.go.plugin.api.GoPlugin;
+import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
+import com.thoughtworks.go.plugin.api.annotation.Extension;
+import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
+import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.DefaultGoApiResponse;
+import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.task.*;
+import org.apache.commons.io.IOUtils;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Extension
+public class DockerRunTask implements GoPlugin {
+
+    public static final String IMAGE_NAME= "ImageName";
+    public static final String ADDITIONAL_OPTIONS = "AdditionalOptions";
+    public static final String SECURE_CONNECTION = "yes";
+    public static final String SECURE_CONNECTION_PROPERTY = "SecureConnection";
+    public static final String REQUEST_TYPE = "-G";
+    public static final String REQUEST_PROPERTY = "RequestType";
+    Logger logger = Logger.getLoggerFor(DockerRunTask.class);
+
+    @Override
+    public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
+    }
+
+    @Override
+    public GoPluginApiResponse handle(GoPluginApiRequest request) throws UnhandledRequestTypeException {
+        if ("configuration".equals(request.requestName())) {
+            return handleGetConfigRequest();
+        } else if ("validate".equals(request.requestName())) {
+            return handleValidation(request);
+        } else if ("execute".equals(request.requestName())) {
+            return handleTaskExecution(request);
+        } else if ("view".equals(request.requestName())) {
+            return handleTaskView();
+        }
+        throw new UnhandledRequestTypeException(request.requestName());
+    }
+
+    private GoPluginApiResponse handleTaskView() {
+        int responseCode = DefaultGoApiResponse.SUCCESS_RESPONSE_CODE;
+        Map view = new HashMap();
+        view.put("displayValue", "Docker Run");
+        try {
+            view.put("template", IOUtils.toString(getClass().getResourceAsStream("/views/docker.run.task.template.html"), "UTF-8"));
+        } catch (Exception e) {
+            responseCode = DefaultGoApiResponse.INTERNAL_ERROR;
+            String errorMessage = "Failed to find template: " + e.getMessage();
+            view.put("exception", errorMessage);
+            logger.error(errorMessage, e);
+        }
+        return createResponse(responseCode, view);
+    }
+
+    private GoPluginApiResponse handleTaskExecution(GoPluginApiRequest request) {
+        DockerRunTaskExecutor executor = new DockerRunTaskExecutor();
+        Map executionRequest = (Map) new GsonBuilder().create().fromJson(request.requestBody(), Object.class);
+        Map config = (Map) executionRequest.get("config");
+        Map context = (Map) executionRequest.get("context");
+
+        Result result = executor.execute(new Config(config), new Context(context), JobConsoleLogger.getConsoleLogger());
+        return createResponse(result.responseCode(), result.toMap());
+    }
+
+    private GoPluginApiResponse handleValidation(GoPluginApiRequest request) {
+        HashMap validationResult = new HashMap();
+        int responseCode = DefaultGoPluginApiResponse.SUCCESS_RESPONSE_CODE;
+        Map configMap = (Map) new GsonBuilder().create().fromJson(request.requestBody(), Object.class);
+        if (!configMap.containsKey(IMAGE_NAME) || ((Map) configMap.get(IMAGE_NAME)).get("value") == null || ((String) ((Map) configMap.get(IMAGE_NAME)).get("value")).trim().isEmpty()) {
+            responseCode = DefaultGoPluginApiResponse.VALIDATION_FAILED;
+            HashMap errorMap = new HashMap();
+            errorMap.put(IMAGE_NAME, "Image name cannot be empty");
+            validationResult.put("errors", errorMap);
+        }
+        return createResponse(responseCode, validationResult);
+    }
+
+    private GoPluginApiResponse handleGetConfigRequest() {
+        HashMap config = new HashMap();
+        config.put(IMAGE_NAME, null);
+
+	// example of putting a default value
+        //HashMap valueForSecureConnectionProperty = new HashMap();
+        //valueForSecureConnectionProperty.put("default-value", SECURE_CONNECTION);
+        //config.put(SECURE_CONNECTION_PROPERTY, valueForSecureConnectionProperty);
+
+        return createResponse(DefaultGoPluginApiResponse.SUCCESS_RESPONSE_CODE, config);
+    }
+
+    private GoPluginApiResponse createResponse(int responseCode, Map body) {
+        final DefaultGoPluginApiResponse response = new DefaultGoPluginApiResponse(responseCode);
+        response.setResponseBody(new GsonBuilder().serializeNulls().create().toJson(body));
+        return response;
+    }
+
+    @Override
+    public GoPluginIdentifier pluginIdentifier() {
+        return new GoPluginIdentifier("task", Arrays.asList("1.0"));
+    }
+}
